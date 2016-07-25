6241d03038f429f6bc36d92d7e7617e3d4bf48e8
tatan100191 <jonathan.amanriqueg@gmail.com>
2016-07-24 20:19:24 -0500

    grafica

--- Vista/cargarDatos.php
+++ Vista/cargarDatos.php
@@ -11,10 +11,20 @@
  * @author Tatan
  */
 require_once ('../Modelo/Calculos.php');
+require_once ('../Modelo/Utilitario.php');
 $calculos = new Calculos();
-$numPrimarios = $_POST['enlacesPrimarios'];
-$numSecundarios = $_POST['enlacesSecundarios'];
- echo json_encode($calculos->cargarDatosEnlaces($numSecundarios, $numPrimarios));
+$utilitario = new Utilitario();
+if($_POST['accion']== 1){
+    $numPrimarios = $_POST['enlacesPrimarios'];
+    $numSecundarios = $_POST['enlacesSecundarios'];
+    echo json_encode($calculos->cargarDatosEnlaces($numSecundarios, $numPrimarios));
+}
+else if($_POST['accion']== 2){
+    $tipoGrafica = $_POST['tipoGrafica'];
+    if ($tipoGrafica == "usuariosXCanal"){
+        echo json_encode($utilitario->usuariosXcanal());
+    }
+}
 
 
 ?>
